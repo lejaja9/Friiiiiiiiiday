7833aba59d328124b5cb4799a412a1c1ffe9c29e
LeJaja9 <jialun.wang32@gmail.com>
2022-03-09 22:26:58 -0800

    test route

--- Backend/app.py
+++ Backend/app.py
@@ -3,13 +3,13 @@ from Spotify_req import *
 
 app = Flask(__name__)
 
-@app.route('/<string:song_id>/<string:activity>/<string:time>/<string:genre>/')
+# @app.route('/<string:song_id>/<string:activity>/<string:time>/<string:genre>/')
 
-def recs(song_id, activity, time, genre):
-    recommendations = song_rec(song_id, activity, time, genre)
-    recommendations.get_song_recommendations()
+# def recs(song_id, activity, time, genre):
+#     recommendations = song_rec(song_id, activity, time, genre)
+#     recommendations.get_song_recommendations()
     
-    return jsonify(recommendations.output)
+#     return jsonify(recommendations.output)
 
 
 @app.route('/')
