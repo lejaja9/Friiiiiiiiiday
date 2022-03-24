dcac80027a3167dc067b43386f553240c94f5516
martin-stoyanov <martin.a.stoyanov@gmail.com>
2022-03-23 20:50:06 -0400

    actually make modal image display

--- frontend/components/SongPreferencesForm.tsx
+++ frontend/components/SongPreferencesForm.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { InformationCircleIcon } from '@heroicons/react/outline';
 import { DropDown } from './Dropdown';
 import { Modal } from './Modal';
+import ModalImage from '../public/images/ModalImage.png';
 
 export const SongPreferencesForm = () => {
   return (
@@ -47,7 +48,7 @@ bg-gray-50 relative px-1  top-2 left-3 w-auto group-focus-within:text-red-600 tr
                       </ul>
                     </p>
                     <Image
-                      src={`/images/ModalImage.png`}
+                      src={ModalImage}
                       width={400}
                       height={350}
                       alt="Right click a song on Spotify, then click 'Share',
